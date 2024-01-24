9b236245a9948b88a4b01bc837bca71f1ce4736c
AylanaQueiroz <aylanaqueiroz@gmail.com>
2024-01-24 18:33:02 -0300

    Jogo dos numeros -  Funções com parâmetros

--- projeto2/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ projeto2/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -25,9 +25,31 @@ titulo.innerHTML = 'Jogo do número Secreto';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
 
-*/
+
+let numeroDoChute = document.querySelector('.container__input')
+
+
+
+//ex1. 
+
+function ola(){
+    console.log('olá, mundo!')
+}
+ola()*/
+ // ex2
+
+ function ola(name) {
+
+    console.log ('olá')
+
+ }
+ola()
+
+
 
 let numeroSecreto = gerarUmNumeroAleatorio();
+let numeroDoChute = document.querySelector('input').value;
+
 
 
 function exibirTextoNaTela(tag, texto) {
@@ -40,17 +62,38 @@ exibirTextoNaTela('h1', 'Jogo do número Secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 
-function gerarUmNumeroAleatorio(){
+function gerarUmNumeroAleatorio() {
 
- return parseInt(Math.random() * 10 +1)
-
+    return parseInt(Math.random() * 10 + 1)
+    console.log(numeroSecreto)
 }
+
 
 
 function verificarChute() {
 
-    console.log(numeroSecreto)
+    let numeroDoChute = document.querySelector('input').value;
+    numeroDoChute.value = ' ';
+
+
+    //console.log('O número secreto é: ' + numeroSecreto)
+
+    //console.log('O número que você chutou foi: ' + numeroChute)
+
+
+    if (numeroDoChute == numeroSecreto) {
+
+        alert("você acertou o número secreto: " + numeroSecreto)
+
+    } else if (numeroDoChute > numeroSecreto) {
+
+        alert("O número secreto é menor que " + numeroDoChute)
+
+    } else {
+        alert("O número secreto é maior que " + numeroDoChute)
+    }
 }
+
 
 
 
